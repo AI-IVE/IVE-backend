81ca5950a502617cd36529365ebfb1a121e72e0f
yujin0131 <lyujin0131@naver.com>
2024-04-05 13:14:29 +0900

    Create IVE-backend Project

---
+++ ive/src/main/java/com/backend/ive/IveApplication.java
@@ -0,0 +1,14 @@
+package com.backend.ive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IveApplication.class, args);
+		System.out.println("######### test ###########");
+	}
+
+}
---
+++ ive/src/test/java/com/backend/ive/IveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
